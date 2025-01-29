4d1f4c3b454b0cdd3555e971e545d3f70f66f6d5
Sovanyuth Yun <120990791+Sovanyuth@users.noreply.github.com>
2025-01-29 00:42:53 -0500

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/sovan/OneDrive/Desktop/Spring 2025/ECE 3574/matrixCalc
+# Build directory: C:/Users/sovan/OneDrive/Desktop/Spring 2025/ECE 3574/matrixCalc/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
